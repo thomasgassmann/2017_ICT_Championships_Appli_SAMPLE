2fed73dd171c7e1ebdf883eba5369956d638de34
Thomas Gassmann <thomas.gassmann@hotmail.com>
2020-08-15 15:30:03 +0200

    whatever, don't really hany any motivation
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- EUFA/EUFA/AddEditTournament.Designer.cs
+++ EUFA/EUFA/AddEditTournament.Designer.cs
@@ -34,6 +34,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.dateStart = new EUFA.Controls.DateOnlyPicker();
             this.dateEnd = new EUFA.Controls.DateOnlyPicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // label1
@@ -88,11 +91,35 @@
             this.dateEnd.Size = new System.Drawing.Size(200, 31);
             this.dateEnd.TabIndex = 7;
             // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(207, 200);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(540, 585);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 158;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Width = 355;
+            // 
             // AddEditTournament
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 832);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.dateEnd);
             this.Controls.Add(this.dateStart);
             this.Controls.Add(this.label3);
@@ -114,5 +141,8 @@
         private System.Windows.Forms.Label label3;
         private Controls.DateOnlyPicker dateStart;
         private Controls.DateOnlyPicker dateEnd;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
