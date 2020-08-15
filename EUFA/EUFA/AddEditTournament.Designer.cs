0f4905ae98fe635f48ae04de2846c301c5dc19f6
Thomas Gassmann <thomas.gassmann@hotmail.com>
2020-08-15 22:08:05 +0200

    Bunch of unproductiveness masked as a commit
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- EUFA/EUFA/AddEditTournament.Designer.cs
+++ EUFA/EUFA/AddEditTournament.Designer.cs
@@ -37,12 +37,13 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Location = new System.Drawing.Point(12, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(189, 25);
             this.label1.TabIndex = 0;
@@ -50,7 +51,7 @@
             // 
             // tbTournamentName
             // 
-            this.tbTournamentName.Location = new System.Drawing.Point(207, 73);
+            this.tbTournamentName.Location = new System.Drawing.Point(415, 83);
             this.tbTournamentName.Name = "tbTournamentName";
             this.tbTournamentName.Size = new System.Drawing.Size(240, 31);
             this.tbTournamentName.TabIndex = 1;
@@ -58,7 +59,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Location = new System.Drawing.Point(12, 142);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 25);
             this.label2.TabIndex = 4;
@@ -67,7 +68,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(431, 138);
+            this.label3.Location = new System.Drawing.Point(639, 143);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 25);
             this.label3.TabIndex = 5;
@@ -77,7 +78,7 @@
             // 
             this.dateStart.CustomFormat = "dd.MM.yy";
             this.dateStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateStart.Location = new System.Drawing.Point(207, 133);
+            this.dateStart.Location = new System.Drawing.Point(415, 138);
             this.dateStart.Name = "dateStart";
             this.dateStart.Size = new System.Drawing.Size(200, 31);
             this.dateStart.TabIndex = 6;
@@ -86,39 +87,54 @@
             // 
             this.dateEnd.CustomFormat = "dd.MM.yy";
             this.dateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateEnd.Location = new System.Drawing.Point(547, 132);
+            this.dateEnd.Location = new System.Drawing.Point(755, 137);
             this.dateEnd.Name = "dateEnd";
             this.dateEnd.Size = new System.Drawing.Size(200, 31);
             this.dateEnd.TabIndex = 7;
             // 
             // listView1
             // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listView1.CheckBoxes = true;
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
             this.listView1.FullRowSelect = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(207, 200);
+            this.listView1.Location = new System.Drawing.Point(415, 200);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(540, 585);
+            this.listView1.Size = new System.Drawing.Size(693, 739);
             this.listView1.TabIndex = 8;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
+            this.columnHeader1.Text = "Participation";
             this.columnHeader1.Width = 158;
             // 
             // columnHeader2
             // 
+            this.columnHeader2.Text = "Team";
             this.columnHeader2.Width = 355;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Participating Teams";
             // 
             // AddEditTournament
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(815, 832);
+            this.ClientSize = new System.Drawing.Size(1120, 951);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.dateEnd);
             this.Controls.Add(this.dateStart);
@@ -144,5 +160,6 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label4;
     }
 }
